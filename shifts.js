f0849b40c28162d9d77395418e840f784a18882b
Brent Coraler <brentcoraler@gmail.com>
2026-09-21 22:44:21 +0800

    Add overtime pay for shifts over 8 hours - coraler.brent

--- shifts.js
+++ shifts.js
@@ -3,7 +3,12 @@ function isValidShift(hours) {
 }
 
 function calculatePay(hours, rate) {
-  return Math.floor(hours * rate);
+    if (hours > 8) {
+        const regularHours = 8;
+        const overtimeHours = hours - 8;
+        return Math.floor((regularHours * rate) + (overtimeHours * rate * 1.5));
+    }
+    return Math.floor(hours * rate);
 }
 
 module.exports = { isValidShift, calculatePay };
